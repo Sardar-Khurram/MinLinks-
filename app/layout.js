83371558fb2868d0feb0334b3298e9f6aaf987fe
Sardar <sardarkhurram121@gmail.com>
2024-12-15 20:37:07 +0500

    Commit2: created URL shortner app ready to push

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,15 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Head from "next/head";
+import favicon from "@/public/favicon.svg";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -14,15 +17,22 @@ const geistMono = localFont({
 });
 
 export const metadata = {
-  title: "MinLink - Best URL Shortner ",
-  description: " A small URL shortner app that makes easy for you to browse",
+  title: "MinLink - Best URL Shortener",
+  description: "A small URL shortener app that makes it easy for you to browse",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-green-50`}>
-        <Navbar/>
+      <Head>
+        <link rel="shortcut icon" href={favicon.src} type="image/x-icon" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </Head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-green-50`}
+      >
+        <Navbar />
         {children}
       </body>
     </html>
